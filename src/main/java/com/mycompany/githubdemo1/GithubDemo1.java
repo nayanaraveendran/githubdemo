3202c8a76ba8d81f502cba479dc28d700a389f44
User <User@DESKTOP-BAFB5OB>
2025-10-16 10:56:15 +0530

    Github demo java project

---
+++ src/main/java/com/mycompany/githubdemo1/GithubDemo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubdemo1;
+
+/**
+ *
+ * @author User
+ */
+public class GithubDemo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
